e3e79c715bb99df102288470426b25864d1f7d00
YinXinLION <yinxin19950816@gmail.com>
2017-05-18 23:40:58 +0800

    倒数第K个

---
+++ jingdian/listK.java
@@ -0,0 +1,30 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+//寻找倒数第K个节点
+public class Solution {
+    public ListNode FindKthToTail(ListNode head,int k) {
+		if(head == null)
+            return head;
+        ListNode node = head;
+        int count = 0;
+        while(node != null){
+            count++;
+            node = node.next;
+        }
+        if(count < k) return null;
+        ListNode p = head;
+        for(int i = 0; i < count - k; i++){
+            p = p.next;
+            
+        }
+        return p;
+            
+    }
+}
